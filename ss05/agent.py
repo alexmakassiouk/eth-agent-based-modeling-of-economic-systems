6b9cca6ab7036e7a0b0c1b6e3c1eb8cc73e5b1c3
ComputasAlex <almax@computas.com>
2024-03-19 19:57:05 +0100

    ss05 task 1-4 done

--- ss05/agent.py
+++ ss05/agent.py
@@ -1,3 +1,4 @@
+from typing import Literal
 import numpy as np
 from mesa.agent import Agent
 
@@ -13,21 +14,35 @@ class PolyaAgent(Agent):
 
         super().__init__(unique_id=unique_id, model=model)
 
-        self.preference = ...
-        self.state = ...
+        self.preference = preference
+        self.state: Literal[1] | Literal[0] = self.choose_state(model)
 
-    def choose_state(self, model):
+    def choose_state(self, model) -> Literal[1] | Literal[0]:
         # The agent choses its state depending on the model type
         model_type = model.model_type
         if model_type == "linear":
-                return self.linear(model.global_frequency)
+            return self.linear(model.global_frequency)
+        elif model_type == "nonlinear":
+            return self.nonlinear(model.global_frequency)
+        else:
+            return 0
 
 
-    def linear(self, f_1):
+    def linear(self, f_1) -> Literal[1] | Literal[0]:
         # Use slide 5
-        ...
+        rnd = self.random.random()
+        return 1 if rnd < f_1 else 0
 
 
-    def nonlinear(self, f_1):
+    def nonlinear(self, f_1) -> Literal[1] | Literal[0]:
         # Use equation and parameters on slide 11
-        ...
+        beta = 10
+        rnd = self.random.random()
+        z = f_1-0.5
+        decision = 1/(1+np.exp(-beta * z))
+        # decision = expit(-beta*z)
+        return 1 if rnd < decision else 0
+    
+    def step(self):
+        pass
+        
--- ss05/model.py
+++ ss05/model.py
@@ -2,9 +2,8 @@ import numpy as np
 from mesa.model import Model
 from mesa.space import SingleGrid
 from mesa.time import BaseScheduler
+from mesa.datacollection import DataCollector
 from agent import PolyaAgent
-
-np.random.seed(42)
 
 
 class PolyaProcess(Model):
@@ -21,7 +20,7 @@ class PolyaProcess(Model):
         Args:
             model_type: "linear", "nonlinear", or "utility"
             num_agents: Number of agents in the model
-            preference_match: a 2x2 array-like structure, denoted as u^c(a, b) in slides where a, b \in {0, 1}.
+            preference_match: a 2x2 array-like structure, denoted as u^c(a, b) in slides where a, b in {0, 1}.
             benefits: a 2d vector representing benefits for agents, denoted as A^0 or A^1 in the slides
         """
         assert model_type in PolyaProcess.MODEL_TYPES,\
@@ -32,22 +31,27 @@ class PolyaProcess(Model):
 
         self.max_steps = max_steps
         self.model_type = model_type
+        self.frequencies = []
         if model_type == "linear" or model_type =="nonlinear":
             # For "linear" or "nonlinear" models,
             # we initialize an agent with state 1 ..
             self.num_ones = 1
             self.num_zeros = 0
-            agent_one = ...
+            agent_one = PolyaAgent(1, self)
             self.schedule.add(agent_one)
             # and an agent with state 0 at t=0
-            self.num_ones = ...
-            self.num_zeros = ...
-            agent_zero = ...
+            self.num_ones = 0
+            self.num_zeros = 1
+            agent_zero = PolyaAgent(0, self)
             self.schedule.add(agent_zero)
             #
             self.num_ones = 1
             self.num_zeros = 1
             self._next_agent_id = 2
+
+            self.datacollector = DataCollector(
+            model_reporters={"f_1": self.collect_frequencies}
+        )
 
             
 
@@ -56,9 +60,18 @@ class PolyaProcess(Model):
     @property
     def global_frequency(self):
         # global_frequency represents N^1 / N
-        ...
+        return self.num_ones/(self.num_ones + self.num_zeros)
 
 
     def step(self):
         '''Advance the model by one step.'''
-        ...
+        new_agent = PolyaAgent(self._next_agent_id, self)
+        self.num_ones += new_agent.state
+        self.num_zeros += 1 if new_agent.state == 0 else 0
+        self._next_agent_id +=1
+        self.frequencies.append(self.global_frequency)
+        self.datacollector.collect(self)
+
+    def collect_frequencies(self):
+         return self.frequencies
+        
