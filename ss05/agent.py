c5c87fa67477aaef944bdf3483d09564dd4f9d83
ComputasAlex <almax@computas.com>
2024-03-20 14:16:53 +0100

    task 5-6 done

--- ss05/agent.py
+++ ss05/agent.py
@@ -46,3 +46,36 @@ class PolyaAgent(Agent):
     def step(self):
         pass
         
+class UtilityPolyaAgent(PolyaAgent):
+    def __init__(self, unique_id, model, utility_type, A, uc, preference=None):
+        assert utility_type in ("preference", "technology")
+        
+        super().__init__(unique_id, model, preference)
+        self.mu = self.random.choice([0,1])
+        self.A: list[float] = A
+        self.uc: list[list[float]] = uc
+        self.utility_type = utility_type
+
+        # self.random.seed(self.unique_id)
+        self.state: int = self.choose_utility_state(model, self.utility_type)
+
+    def calc_preference_utility(self, n):
+        u0 = self.uc[0][self.mu] + self.A[self.mu]*n[0]
+        u1 = self.uc[1][self.mu] + self.A[self.mu]*n[1]
+        self.u = [u0, u1]
+
+    def calc_technology_utility(self, n):
+        u0 = self.uc[0][self.mu] + self.A[0]*n[0]
+        u1 = self.uc[1][self.mu] + self.A[1]*n[1]
+        self.u = [u0, u1]
+
+    # Override
+    def choose_utility_state(self, model, utility_type):
+        n1 = model.num_ones
+        n0 = model.num_zeros
+        if utility_type == "preference":
+            self.calc_preference_utility([n0, n1])
+        elif utility_type == "technology":
+            self.calc_technology_utility([n0, n1])
+        return int(np.argmax(self.u))
+    
--- ss05/model.py
+++ ss05/model.py
@@ -3,7 +3,7 @@ from mesa.model import Model
 from mesa.space import SingleGrid
 from mesa.time import BaseScheduler
 from mesa.datacollection import DataCollector
-from agent import PolyaAgent
+from agent import PolyaAgent, UtilityPolyaAgent
 
 
 class PolyaProcess(Model):
@@ -60,14 +60,17 @@ class PolyaProcess(Model):
     @property
     def global_frequency(self):
         # global_frequency represents N^1 / N
-        return self.num_ones/(self.num_ones + self.num_zeros)
+        if (self.num_ones + self.num_zeros == 0):
+            return 0
+        else:
+            return self.num_ones/(self.num_ones + self.num_zeros)
 
 
     def step(self):
         '''Advance the model by one step.'''
         new_agent = PolyaAgent(self._next_agent_id, self)
         self.num_ones += new_agent.state
-        self.num_zeros += 1 if new_agent.state == 0 else 0
+        self.num_zeros += (1-new_agent.state)
         self._next_agent_id +=1
         self.frequencies.append(self.global_frequency)
         self.datacollector.collect(self)
@@ -75,3 +78,44 @@ class PolyaProcess(Model):
     def collect_frequencies(self):
          return self.frequencies
         
+
+class UtilityPolyaProcess(PolyaProcess):
+    UTILITY_MODELS = ("preference", "technology")
+    MODEL_TYPES = ("linear", "nonlinear", "preference-utility", "technology-utility")
+    def __init__(self, model_type, max_steps, utility_model_type, A, uc, seed, preference_match=None, benefits=None):
+        assert utility_model_type in UtilityPolyaProcess.UTILITY_MODELS
+        assert model_type in UtilityPolyaProcess.MODEL_TYPES
+
+        super().__init__(model_type, max_steps, preference_match, benefits)
+        self.utility_type = utility_model_type
+        self.num_zeros = 0
+        self.num_ones = 0
+        self.seed = seed
+        self._next_agent_id = seed
+        self.A = A
+        self.uc = uc
+        self.datacollector = DataCollector(
+            model_reporters={"f_1": self.collect_frequency, "delta_n": self.collect_delta, "n0": self.collect_n_0, "n1": self.collect_n_1}
+        )
+
+    # Override
+    def step(self):
+        new_agent = UtilityPolyaAgent(self._next_agent_id, self, self.utility_type, self.A, self.uc)
+        self.num_ones += new_agent.state
+        self.num_zeros += (1-new_agent.state)
+        self._next_agent_id +=1
+        self.datacollector.collect(self)
+
+
+    @property
+    def delta_n(self):
+        return self.num_zeros-self.num_ones
+    
+    def collect_delta(self):
+         return self.delta_n
+    def collect_frequency(self):
+         return self.global_frequency
+    def collect_n_0(self):
+        return self.num_zeros
+    def collect_n_1(self):
+        return self.num_ones
